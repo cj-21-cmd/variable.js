d9c95b90e4f0321b678ec96f124069eef5297ffc
cjdelacruz <cjdelacruz0521@gmail.com>
2024-09-11 22:36:14 +0700

    Initial commit with variables.js

---
+++ variables.js
@@ -0,0 +1,12 @@
+console.log('Hello World');
+let name = "Cassandra Jade H. Dela CRuz";
+console.log('My name is' + name);
+
+let whatDoYouWntToBeInYourlife = "Programmer";
+console.log("What do you wanna become in your life:",whatDoYouWntToBeInYourlife );
+
+let gender = "Female";
+console.log("gender:", gender);
+
+let twitterHandle = "cjadedelacruz2";
+console.log("TwitterHandle:", twitterHandle);
